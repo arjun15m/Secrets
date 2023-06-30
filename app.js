daebddfa44406f25896e69a2860d2419542c5cd5
Arjun <arjunmaheshwari159@gmail.com>
2023-06-30 12:37:50 +0530

    Level two security

--- app.js
+++ app.js
@@ -3,61 +3,65 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema = {
-    email: String,
-    password: String
-};
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = "Thisisourlittlesecret."; //defining secret to use encryption
+userSchema.plugin(encrypt, { secret: secret , encryptedFields: ["password"] }); 
 
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
-    res.render("home");
+  res.render("home");
 });
 
 app.get("/login", function (req, res) {
-    res.render("login");
+  res.render("login");
 });
 
 app.get("/register", function (req, res) {
-    res.render("register");
+  res.render("register");
 });
 
-app.post("/register", async function(req, res){
+app.post("/register", async function (req, res) {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password
   });
 
-  try{
+  try {
     await newUser.save();
     res.render("secrets");
-  }catch(e){
+  } catch (e) {
     console.log(e);
   }
 });
 
-app.post("/login", async function(req, res){
-  const username =  req.body.username;
+app.post("/login", async function (req, res) {
+  const username = req.body.username;
   const password = req.body.password;
 
-  try{
-    const foundUser = await User.findOne({email: username});
-    if(foundUser){
-        if(foundUser.password === password){
-            res.render("secrets");
-        }
+  try {
+    const foundUser = await User.findOne({ email: username });
+    if (foundUser) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
     }
-  }catch(err){
+  } catch (err) {
     console.log(err);
   }
 });
@@ -79,5 +83,5 @@ app.post("/login", async function(req, res){
 
 
 app.listen(3000, function () {
-    console.log("server started at port 3000");
+  console.log("server started at port 3000");
 });
